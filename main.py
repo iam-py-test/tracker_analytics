9e307a4ddc40d89053072f252868c42536cf8bd7
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-10-21 07:44:56 -0400

    Adding per-domain stats

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from urllib.parse import urlparse
 
 # setup
 t = Tranco(cache=True, cache_dir='.tranco')
-latest_top = t.list().top(10)
+latest_top = t.list().top(15)
 trackerdomains = {"Google":["google-analytics.com","ssl.google-analytics.com","www.google-analytics.com","www-google-analytics.l.google.com","googletagmanager.com","www.googletagmanager.com","static-doubleclick-net.l.google.com","www-googletagmanager.l.google.com","ssl-google-analytics.l.google.com","googlesyndication.com","wwwctp.googletagmanager.com","wp.googletagmanager.com","googletagservices.com","www.googletagservices.com"]}
 malwaredomains = requests.get("https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt").text.split("\n")
-data = {"domains_tested":0,"domains_with_tracker":0,"domains_with_HTTPS":0}
+data = {"domains_tested":0,"domains_with_tracker":0,"domains_with_HTTPS":0,"per_domain_stats":{}}
 
 # functions
 def hasHTTPS(domain):
@@ -47,8 +47,10 @@ for domain in latest_top:
       domainreport = hastrackers(req.text)
       if domainreport["has_trackers"] == True:
         data["domains_with_tracker"] += 1
-      if hasHTTPS(domain):
+      hassec = hasHTTPS(domain)
+      if hassec:
         data["domains_with_HTTPS"] += 1
+      data["per_domain_stats"][domain] = {"hasHTTPS":hassec,"has_trackers":domainreport["has_trackers"]}
     except:
       pass
 
@@ -57,4 +59,10 @@ with open("report.md","w") as f:
   f.write("{} domains tested\n".format(data["domains_tested"]))
   f.write("{} of the domains tested used known trackers\n".format(data["domains_with_tracker"]))
   f.write("{} of the domains tested supported HTTPS\n".format(data["domains_with_HTTPS"]))
+  f.write("\n\n### Individual domain statistics: ")
+  
+  for entry in data["per_domain_stats"]:
+    f.write("\n\n#### {}".format(entry))
+    f.write("HTTPS: {}".format(data["per_domain_stats"][entry]["hasHTTPS"]))
+    f.write("Known trackers: {}".format(data["per_domain_stats"][entry]["has_trackers"]))
   f.close()
